fb1c4407ef918f42a7f9bc7cc3f91edb14447528
kerrongordon <kgpsounds.com@gmail.com>
2019-12-15 14:32:16 -0400

    update to 3.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_simple_video_player/flutter_simple_video_player.dart';
 
-void main() => runApp(Gbnlive());
+final gbnUrl = "http://gbn.gd:8080/gbnlive/livestream/index.m3u8";
 
-class Gbnlive extends StatelessWidget {
-
-  final gbnUrl = "http://gbn.gd:8080/gbnlive/livestream/index.m3u8";
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        child: SimpleViewPlayer(gbnUrl, isFullScreen: true,),
-      ),
-    );
-  }
-}
+void main() => runApp(MaterialApp(
+  home: SimpleViewPlayer(gbnUrl, isFullScreen: true),
+));
