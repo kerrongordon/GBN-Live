dc4b07a99112ac99decc1f4ffce84bcddcad8eba
kerron gordon <kgpsounds.com@gmail.com>
2020-09-15 20:21:07 -0400

    change url

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_simple_video_player/flutter_simple_video_player.dart';
 
-final gbnUrl = "http://gbn.gd:8080/gbnlive/livestream/index.m3u8";
+final gbnUrl = "https://gbn.gd:5443/LiveApp/streams/livestream.m3u8";
 
 void main() => runApp(MaterialApp(
   home: SimpleViewPlayer(gbnUrl, isFullScreen: true),
